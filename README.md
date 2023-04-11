562a74ec4ea47dc98e04c31844882aec87c66e00
Durban86 <durban86@verizon.net>
2023-04-11 15:22:08 -0400

    new design and updated JS to push to array

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 https://www.theodinproject.com/lessons/node-path-javascript-library
 
-Create a library using HTML, CSS, and JS. 
+Create a desktop library using HTML, CSS, and JS.
 
 3/31/23 - Begin project
+
+Project will not contain any storage
--- script.js
+++ script.js
@@ -1,12 +1,44 @@
+const form = document.getElementById('new-book-form');
+
+let myLibrary = [{
+    title: 'Ready Player One',
+    author: 'Ernest Cline',
+    pages: 374,
+    read: true
+},
+{
+    title: 'Ready Player Two',
+    author: 'Ernest Cline',
+    pages: 366,
+    read: false
+}
+];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + read
-    }
 }
 
-const theHobbit = new Book('The Hobbit','J.R.R. Tolkien', 295, 'not yet read')
-console.log(theHobbit.info());
+function addBookToLibrary(e) {
+    e.preventDefault();
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value 
+    let pages = document.getElementById('pages').value 
+    let read = document.getElementById('read').value
+    let book = new Book(title, author, pages, read)
+    myLibrary.push(book)
+    form.reset();
+}
+
+const addBtn = document.getElementById('add')
+addBtn.addEventListener('click', addBookToLibrary)
+// function displayBooks() {
+//     for (i=0; i<myLibrary.length; i++){
+//         titleCell.innerText = myLibrary[i].title;
+//         authorCell.innerText = myLibrary[i].author;
+//         pagesCell.innerText = myLibrary[i].pages;
+//         readCell.innerText = myLibrary[i].read;
+//     }
+// }
